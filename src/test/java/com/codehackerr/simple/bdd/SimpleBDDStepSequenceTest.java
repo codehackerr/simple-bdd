b96336d8281687e4473b2ce4e75a927016c64f0f
codehackerr <asalam@aconex.com>
2017-09-08 06:27:25 -0700

    Remove unused code

--- src/test/java/com/codehackerr/simple/bdd/SimpleBDDStepSequenceTest.java
+++ src/test/java/com/codehackerr/simple/bdd/SimpleBDDStepSequenceTest.java
@@ -7,8 +7,6 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static com.codehackerr.simple.bdd.SimpleBDD.given;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.inOrder;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -18,7 +16,7 @@ public class SimpleBDDStepSequenceTest {
     private TestSteps steps;
 
     @Test
-    public void steps_sequential_invocation() {
+    public void steps_invocation() {
 
         given("Step 1",
                 () -> steps.step1()
@@ -32,28 +30,6 @@ public class SimpleBDDStepSequenceTest {
         stepInvocationOrder.verify(steps).step1();
         stepInvocationOrder.verify(steps).step2();
         stepInvocationOrder.verify(steps).step3();
-    }
-
-    @Test
-    public void string_building() {
-        String hello = "hello",
-                world = "world";
-        StringBuilder helloWorld = new StringBuilder();
-
-        given("Step 1", () -> {
-
-            assert hello.length() != 0;
-            assert world.length() != 0;
-
-        }).when("Step 2", () -> {
-
-            helloWorld.append(hello).append(world);
-
-        }).then("Step 3", () -> {
-
-            assertThat(helloWorld.toString(), is("helloworld"));
-
-        });
     }
 
     interface TestSteps {
