773970cd19340e8947d930873a25e4be35e31865
codehackerr <asalam@aconex.com>
2017-09-13 21:55:55 -0700

    Add integration tests skeleton

---
+++ src/integration-test/java/com/codehackerr/simple/bdd/SimpleBDDIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.codehackerr.simple.bdd;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public class SimpleBDDIntegrationTest {
+    @org.junit.Test
+    public void test() {
+        org.junit.Assert.assertThat(true, is(true));
+    }
+}
